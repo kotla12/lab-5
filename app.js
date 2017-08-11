4d3e691c5e79bfb1c7d95f7893881e0da4006812
kotla12 <kotla9119@gmail.com>
2017-08-11 15:53:11 -0700

    question 4 is done

--- app.js
+++ app.js
@@ -49,7 +49,9 @@ Test this function by hand in the console to get it working, and when you think 
 
 function sumAndMultiply(a,b,c){ //eslint-disable-line
   var addSumOne = sum(a,b)[0];
+  console.log (addSumOne);
   var addSumTwo = sum(addSumOne,c)[0];
+  console.log(addSumTwo);
   var productOne = multiply(a,b)[0];
   var productTwo = multiply(productOne,c)[0];
   var outputOne = a + ' and ' + b + ' and ' + c + ' sum to ' + addSumTwo + '.';
@@ -73,11 +75,19 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2,3,4]; //eslint-disable-line
 
 function sumArray(testArray){ //eslint-disable-line
-
+  var count = 0;
+  for (var i = 0 ; i < testArray.length ; i++)
+  {
+    count = sum(testArray[i], count)[0];
+    console.log(count);
+  }
+  var output = testArray[0] + ',' + testArray[1] + ',' + testArray[2] + ' was passed in as an array of numbers, and ' + count + ' is their sum.';
+  console.log(output);
+  return [count, output];
 }
 
 // TODO: Here is the test for sumArray(); uncomment it to run it
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
